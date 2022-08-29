6ae6f0ba90fc3535c3fdb9d5d22c35e932d00253
Abhishek <you@example.com>
2022-08-29 19:24:18 +0530

    [Abhishek] Added: UC1 creaing a simple linkedList

---
+++ Day14_LinkedList/src/com/bridgelabz/linkedList/LinkedListMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.linkedList;
+/*
+* @author - Abhishek Kumar
+* since - 29-08-2022
+* */
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(56);
+        linkedList.addFirst(30);
+        linkedList.addFirst(70);
+        linkedList.printlist();
+    }
+
+}
